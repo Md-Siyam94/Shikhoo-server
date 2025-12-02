f9e186f97d8ff74f7463efcc6df853fd0ba97a27
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-12-02 17:53:52 +0600

    get and post api done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
+const User = require('../models/User.model');
 const router = express.Router()
 
 
 
+  // post user
+    router.post('/', async(req, res)=>{
+        const user = new User(req.body);
+        const filter = {email: user?.email}
+        const existingUser = await User.findOne(filter);
+        if(existingUser){
+            return res.json({message: "user already have an account, please login!"})
+        }
+        await user.save();
+        return res.status(201).json({success: true, data: user})
+    })
+
+    // get all users
+    router.get('/', async(req,res)=>{
+        const users = await User.find();
+        return res.json(users)
+    })
+
+    // get 
+
+  
 
 module.exports = router
